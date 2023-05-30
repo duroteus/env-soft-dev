e2e383f0a87c571ee51668412e62f7ddff72b490
francisco <f.rodriguesbneto@gmail.com>
2023-05-30 12:40:59 -0300

    Inserindo a classe disciplina

---
+++ src/Disciplina.java
@@ -0,0 +1,37 @@
+public class Disciplina {
+
+    private String nome;
+    private Integer codigo;
+    private Integer cargaHoraria;
+
+    public Disciplina(String nome, Integer codigo, Integer cargaHoraria) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+}
